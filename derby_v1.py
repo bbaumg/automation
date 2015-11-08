4fe6cc0ee264d03e68fd9983727b54eca6840931
Barrett <bbaumg@gmail.com>
2015-11-08 01:00:01 -0600

    upload

--- derby_v1.py
+++ derby_v1.py
@@ -1,11 +1,11 @@
-
 #!/usr/bin/env python3
-# script by Alex Eames http://RasPi.tv
-# http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
 import RPi.GPIO as GPIO
 import sys
 import time
+import inspect
 from datetime import datetime
+from Adafruit_7Segment import SevenSegment
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -19,49 +19,91 @@ GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+segment = SevenSegment(address=0x70)
 
 def gpio_18(channel):
-    end_time_18 = (datetime.now() - start_time)
-    print "Pin 18 = " + str(end_time_18)
+  global start_time
+  finishOrder("START")
+  start_time = datetime.now()
+  print "Start of race = " + str(start_time)
 
 def gpio_23(channel):
-    end_time_23 = (datetime.now() - start_time)
-    print "Pin 23 = " + str(end_time_23)
+  lane = 2
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_24(channel):
-    end_time_24 = (datetime.now() - start_time)
-    print "Pin 24 = " + str(end_time_24)
+  lane = 3
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_25(channel):
-    end_time_25 = (datetime.now() - start_time)
-    print "Pin 25 = " + str(end_time_25)
+  lane = 4
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_4(channel):
-    end_time_4 = (datetime.now() - start_time)
-    print "Pin 4 = " + str(end_time_4)
+  lane = 5
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_17(channel):
-    end_time_17 = (datetime.now() - start_time)
-    print "Pin 17 = " + str(end_time_17)
+  lane = 6
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_14(channel):
-    end_time_14 = (datetime.now() - start_time)
-    print "Pin 14 = " + str(end_time_14)
-    
+  lane = 7
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
+
 def gpio_15(channel):
-    end_time_15 = (datetime.now() - start_time)
-    print "Pin 15 = " + str(end_time_15)
+  lane = 8
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + lane[-1:] + " (" + end_time + ")"
 
 def gpio_8(channel):
-    end_time_8 = str((datetime.now() - start_time))
-    end_time_8 = end_time_8[::2]
-    print "Pin 8 = " + end_time_8
+  lane = 9
+  end_time = str((datetime.now() - start_time))[5:12]
+  place = finishOrder(lane)
+  print place + " = Lane " + str(lane) + " (" + end_time + ")"
 
-def gpio_7(channel):
-    global start_time
-    start_time = datetime.now()
-    print "Start of race = " + str(start_time)
+def finishOrder(lane):
+  global p1st
+  global p2nd
+  global p3rd
+  global p4th
+  if lane == "START":
+    p1st = ""
+    p2nd = ""
+    p3rd = ""
+    p4th = ""
+    segment.writeDigit(0, int(lane))
+  elif p1st == "":
+    p1st = lane
+    segment.writeDigit(0, int(lane))
+    return "1st Place"
+  elif p2nd == "":
+    p2nd = lane
+    #segment.writeDigit(1, int(lane))
+    return "2nd Place"
+  elif p3rd == "":
+    p3rd = lane
+    #segment.writeDigit(3, int(lane))
+    return "3rd Place"
+  elif p4th == "":
+    p4th = lane
+    #segment.writeDigit(4, int(lane))
+    return "4th Place"
+  else:
+    return "ERROR - Too Many Lanes"
 
 GPIO.add_event_detect(18, GPIO.FALLING, callback=gpio_18, bouncetime=3000)
 GPIO.add_event_detect(23, GPIO.FALLING, callback=gpio_23, bouncetime=3000)
@@ -69,17 +111,17 @@ GPIO.add_event_detect(24, GPIO.FALLING, callback=gpio_24, bouncetime=3000)
 GPIO.add_event_detect(25, GPIO.FALLING, callback=gpio_25, bouncetime=3000)
 GPIO.add_event_detect(4, GPIO.FALLING, callback=gpio_4, bouncetime=3000)
 GPIO.add_event_detect(17, GPIO.FALLING, callback=gpio_17, bouncetime=3000)
-GPIO.add_event_detect(14, GPIO.FALLING, callback=gpio_14, bouncetime=3000)
-GPIO.add_event_detect(15, GPIO.FALLING, callback=gpio_15, bouncetime=3000)
-GPIO.add_event_detect(8, GPIO.FALLING, callback=gpio_8, bouncetime=3000)
-GPIO.add_event_detect(7, GPIO.FALLING, callback=gpio_7, bouncetime=10000)
+#GPIO.add_event_detect(14, GPIO.FALLING, callback=gpio_14, bouncetime=3000)
+#GPIO.add_event_detect(15, GPIO.FALLING, callback=gpio_15, bouncetime=3000)
+#GPIO.add_event_detect(8, GPIO.FALLING, callback=gpio_8, bouncetime=3000)
+#GPIO.add_event_detect(7, GPIO.FALLING, callback=gpio_7, bouncetime=10000)
 
 try:
-    print "Waiting for rising edge on port 22"
-    GPIO.wait_for_edge(22, GPIO.FALLING)
-    print "Rising edge detected on port 22. Here endeth the third lesson."
+  print "Waiting for rising edge on port 22"
+  GPIO.wait_for_edge(22, GPIO.FALLING)
+  print "Rising edge detected on port 22. Here endeth the third lesson."
 
 except KeyboardInterrupt:
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+  GPIO.cleanup()       # clean up GPIO on CTRL+C exit
 GPIO.cleanup()           # clean up GPIO on normal exit
 
---
+++ derby_v2.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import sys
+import time
+import signal
+from datetime import datetime
+from Adafruit_7Segment import SevenSegment
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+segment = SevenSegment(address=0x70)
+segment.setColon(0)
+#segment.print_hex(0, '0x40')
+
+def signal_handler(signal, frame):
+    print 'You pressed Ctrl+C!'
+    sys.exit(0)
+
+def raceStart(channel):
+  global dt_RaceStart
+  dt_RaceStart = datetime.now()
+  #print "Start of race = " + str(dt_RaceStart)
+  carFinished("START")
+
+def lane1(channel):
+  lane = 1
+  carFinished(lane)
+
+def lane2(channel):
+  lane = 2
+  carFinished(lane)
+
+def lane3(channel):
+  lane = 3
+  carFinished(lane)
+
+def lane4(channel):
+  lane = 4
+  carFinished(lane)
+
+def lane5(channel):
+  lane = 5
+  carFinished(lane)
+
+def lane6(channel):
+  lane = 6
+  carFinished(lane)
+
+def carFinished(lane):
+  global p1
+  global p2
+  global p3
+  global p4
+  global p5
+  global p6
+  dt_laneEnd = str((datetime.now() - dt_RaceStart))[5:12]
+
+  if lane == "START":
+    p1 = ""
+    p2 = ""
+    p3 = ""
+    p4 = ""
+    p5 = ""
+    p6 = ""
+    print("Let the Race Begin!!!")
+    segment.writeDigitRaw(0, 0x40)
+    segment.writeDigitRaw(1, 0x40)
+    segment.writeDigitRaw(3, 0x40)
+    segment.writeDigitRaw(4, 0x40)
+  elif p1 == "":
+    p1 = lane
+    segment.writeDigit(lane-1, 1)
+    place = "1st Place"
+  elif p2 == "":
+    p2 = lane
+    segment.writeDigit(lane-1, 2)
+    place = "2nd Place"
+  elif p3 == "":
+    p3 = lane
+    segment.writeDigit(lane-2, 3)
+    place = "3rd Place"
+  elif p4 == "":
+    p4 = lane
+    segment.writeDigit(lane-2, 4)
+    place = "4th Place"
+  elif p5 == "":
+    p5 = lane
+    place = "5th Place"
+  elif p6 == "":
+    p6 = lane
+    place = "6th Place"
+  if lane != "START":
+    print("Lane " + str(lane) + " you finished " + place + "(" + dt_laneEnd + ")")
+    if lane == 1:
+    elif lane ==2:
+    elif lane ==3:
+    elif lane ==4:
+
+def get_rank(lane):
+
+
+def get_time(lane):
+
+
+
+GPIO.add_event_detect(18, GPIO.FALLING, callback=raceStart, bouncetime=10000)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=lane1, bouncetime=3000)
+GPIO.add_event_detect(24, GPIO.FALLING, callback=lane2, bouncetime=3000)
+GPIO.add_event_detect(25, GPIO.FALLING, callback=lane3, bouncetime=3000)
+GPIO.add_event_detect(4, GPIO.FALLING, callback=lane4, bouncetime=3000)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=lane5, bouncetime=3000)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=lane6, bouncetime=3000)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+print 'Press Ctrl+C'
+while True:
+    time.sleep(.01)
+
